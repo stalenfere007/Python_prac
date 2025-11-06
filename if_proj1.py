a9f9abf141c1383d398e05f8c515a9387ba1bdc3
stalenfere007 <141028437+stalenfere007@users.noreply.github.com>
2025-11-06 19:41:18 +0530

    Add files via upload

---
+++ if_proj1.py
@@ -0,0 +1,7 @@
+num1 = int(input("Enter a number: "))
+if num1 % 2 == 0:
+    print(f"{num1}is an even number!!!")
+else:
+    print(f"{num1} is an odd number!!!")
+
+print("Thanks for using this program")
