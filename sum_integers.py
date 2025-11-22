d406a0b6ac1e354eefe994a9f3c90959770bad08
stalenfere007 <141028437+stalenfere007@users.noreply.github.com>
2025-11-22 14:58:05 +0530

    Add files via upload

---
+++ sum_integers.py
@@ -0,0 +1,8 @@
+total = 0
+for i in range(1, 51):
+    total = total + i
+
+
+print("*******************************************")
+print(f"The sum of integers from 1 to 50 is {total}")
+print("********************************************")
